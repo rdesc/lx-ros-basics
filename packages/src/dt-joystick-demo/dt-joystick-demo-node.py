4274644e863b97258e9b579b9acaa9695684cb8c
Liam Paull <paulll@iro.umontreal.ca>
2024-02-21 23:14:40 -0500

    small

--- packages/src/dt-joystick-demo/dt-joystick-demo-node.py
+++ packages/src/dt-joystick-demo/dt-joystick-demo-node.py
@@ -4,52 +4,53 @@ import rospy
 from sensor_msgs.msg import Joy
 from duckietown_msgs.msg import WheelsCmdStamped
 
-def __init__(self):
+class DTJoystickDemoNode():
+    def __init__(self):
 
-    self.sub_joy = rospy.Subscriber(
-        "/agent/joy", 
-        Joy,
-        self.process_joy() 
-    )
+        self.sub_joy = rospy.Subscriber(
+            "/agent/joy", 
+            Joy,
+            self.process_joy() 
+        )
 
-    self.pub_wheel_cmds = rospy.Publisher(
-        "/agent/wheels_driver_node/wheels_cmd",
-        WheelsCmdStamped,
-        queue_size=1
-    )
+        self.pub_wheel_cmds = rospy.Publisher(
+            "/agent/wheels_driver_node/wheels_cmd",
+            WheelsCmdStamped,
+            queue_size=1
+        )
 
-def process_joy(self,msg):
-    cmd_to_publish = WheelsCmdStamped()
-    cmd_to_publish.header = msg.header
-    cmd_to_publish.vel_right = 0.0
-    cmd_to_publish.vel_left = 0.0
-    
-    v = 0.5 # this will be the velocity we publish
+    def process_joy(self,msg):
+        cmd_to_publish = WheelsCmdStamped()
+        cmd_to_publish.header = msg.header
+        cmd_to_publish.vel_right = 0.0
+        cmd_to_publish.vel_left = 0.0
+        
+        v = 0.5 # this will be the velocity we publish
 
-    if msg.axes[1] == 1.0:
-        # go forward
-        cmd_to_publish.vel_right = v
-        cmd_to_publish.vel_left = v
+        if msg.axes[1] == 1.0:
+            # go forward
+            cmd_to_publish.vel_right = v
+            cmd_to_publish.vel_left = v
 
-    if msg.axes[1] == -1.0:
-        # go backwards
-        cmd_to_publish.vel_right = -v
-        cmd_to_publish.vel_left = -v
+        if msg.axes[1] == -1.0:
+            # go backwards
+            cmd_to_publish.vel_right = -v
+            cmd_to_publish.vel_left = -v
 
-    if msg.axes[3] == 1.0:
-        # turn right
-        cmd_to_publish.vel_right = -v
-        cmd_to_publish.vel_left = v
+        if msg.axes[3] == 1.0:
+            # turn right
+            cmd_to_publish.vel_right = -v
+            cmd_to_publish.vel_left = v
 
-    if msg.axes[3] == -1.0:
-        # turn left
-        cmd_to_publish.vel_right = v
-        cmd_to_publish.vel_left = -v
+        if msg.axes[3] == -1.0:
+            # turn left
+            cmd_to_publish.vel_right = v
+            cmd_to_publish.vel_left = -v
 
-    self.pub_wheel_cmds.publish(cmd_to_publish)
+        self.pub_wheel_cmds.publish(cmd_to_publish)
 
 if __name__ == "__main__":
     # Initialize the node
-    node = dt-joystick-demo-node()
+    node = DTJoystickDemoNode()
     # Keep it spinning
     rospy.spin()
