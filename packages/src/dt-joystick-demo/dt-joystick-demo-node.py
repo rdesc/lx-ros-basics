919c587a80b6c1db7e46b1b8becb8b131b4ae436
Andrea F. Daniele <afdaniele@duckietown.com>
2024-09-21 16:45:59 -0400

    simplified exercise to work with ente infra

--- packages/src/dt-joystick-demo/dt-joystick-demo-node.py
+++ packages/src/dt-joystick-demo/dt-joystick-demo-node.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python3
 
-import rospy
 import os
-from sensor_msgs.msg import Joy
-from duckietown_msgs.msg import WheelsCmdStamped
 
-class DTJoystickDemoNode():
+import rospy
+from duckietown_msgs.msg import WheelsCmdStamped
+from sensor_msgs.msg import Joy
+
+
+class DTJoystickDemoNode:
     def __init__(self):
-        
-        veh_name = os.environ['VEHICLE_NAME']
+        rospy.init_node("dt_joystick_demo_node")
+
+        veh_name = os.environ["VEHICLE_NAME"]
         self.sub_joy = rospy.Subscriber(
-            f"/{veh_name}/joy", 
+            f"/{veh_name}/joy",
             Joy,
-            self.process_joy
+            self.process_joy,
+            queue_size=1,
         )
 
         self.pub_wheel_cmds = rospy.Publisher(
             f"/{veh_name}/wheels_driver_node/wheels_cmd",
-            WheelsCmdStamped
+            WheelsCmdStamped,
+            queue_size=1,
         )
 
-    def process_joy(self,msg):
+    def process_joy(self, msg):
         cmd_to_publish = WheelsCmdStamped()
         cmd_to_publish.header = msg.header
         cmd_to_publish.vel_right = 0.0
         cmd_to_publish.vel_left = 0.0
-        
-        v = 0.5 # this will be the velocity we publish
+
+        v = 0.5  # this will be the velocity we publish
 
         if msg.axes[1] == 1.0:
             # go forward
@@ -50,9 +55,9 @@ class DTJoystickDemoNode():
 
         self.pub_wheel_cmds.publish(cmd_to_publish)
 
+
 if __name__ == "__main__":
     # Initialize the node
     node = DTJoystickDemoNode()
-    rospy.init_node('dt_joystick_demo_node')
     # Keep it spinning
     rospy.spin()
